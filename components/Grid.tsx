8e353ee268f4130b6e69e4a4612cd49829948e3b
Joseveji96 <joseveji96@gmail.com>
2024-10-17 21:13:31 -0600

    Grid Finalizado

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,8 +6,17 @@ const Grid = () => {
   return (
     <section id='about'>
       <BentoGrid>
-        {gridItems.map((item) =>(
-          <BentoGridItem id={item.id} key={item.id} title={item.title} description={item.description}/>
+        {gridItems.map(({id, title, description, className, imgClassName, titleClassName, img, spareImg}) =>(
+          <BentoGridItem 
+            id={id} 
+            key={id} 
+            title={title} 
+            description={description} 
+            className={className} 
+            imgClassName={imgClassName} 
+            titleClassName={titleClassName} 
+            spareImg={spareImg} 
+            img={img}/>
         ))}
       </BentoGrid>
     </section>
